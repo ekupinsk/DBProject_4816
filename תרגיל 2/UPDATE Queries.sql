435f7fed4712e96cb72a8eb4891d4d6a2d8f76e3
ely <44139691+ekupinsk@users.noreply.github.com>
2025-05-14 12:28:49 +0300

    Update UPDATE Queries.sql

--- תרגיל 2/UPDATE Queries.sql
+++ תרגיל 2/UPDATE Queries.sql
@@ -9,12 +9,29 @@ WHERE employee_id IN (
   HAVING COUNT(o.order_id) < 10
 );
 
+--הדפסה
+SELECT e.employee_id, e.name, e.salary
+FROM Employee e
+LEFT JOIN Orders o ON e.employee_id = o.employee_id
+GROUP BY e.employee_id
+HAVING COUNT(o.order_id) < 10;
+
 -- 2. שינוי סטטוס להזמנות ישנות
 UPDATE Orders
 SET status = 'archived'
 WHERE order_date < CURRENT_DATE - INTERVAL '6 months' AND status = 'open';
 
+--הדפסה
+SELECT order_id, order_date, status
+FROM Orders
+WHERE order_date < CURRENT_DATE - INTERVAL '6 months' AND status = 'open';
+
 -- 3. עדכון זמינות טבלאות שלא נוקו
 UPDATE Restaurant_Table
 SET is_available = 'N'
+WHERE last_cleaned < CURRENT_DATE - INTERVAL '7 days';
+
+--הדפסה
+SELECT table_id, is_available, last_cleaned
+FROM Restaurant_Table
 WHERE last_cleaned < CURRENT_DATE - INTERVAL '7 days';
